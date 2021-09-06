e26b2b5878c5c75f9a8cc4017c223ac335f718f6
Angie Pineda <agpinedam@unal.edu.co>
2021-09-06 11:15:20 -0500

    First test case with page object model

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private By login = By.linkText("Login");
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void Login(){
+        driver.findElement(login).click();
+    }
+}
